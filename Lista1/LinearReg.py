92e7f62d708ddf4222601041efb30cf2f0043752
Samuel Vieira <102123599+samue1v@users.noreply.github.com>
2022-04-26 08:34:30 -0600

    class

---
+++ Lista1/LinearReg.py
@@ -0,0 +1,14 @@
+import numpy
+class LinearReg:
+    def __init__(self,file,range_columns):
+        self.file = file
+        self.range = range_columns
+    def get_dataset():
+        self.dataset = np.genfromtxt(file, delimiter=',', skip_header=0)
+        return dataset
+        
+        
+class OLS(LinearReg):
+    def __init__(self):
+        super()
+    
