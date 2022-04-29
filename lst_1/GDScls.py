9fad525ba44e223f796840c1a25ccb6dbd81a59a
Samuel Vieira <102123599+samue1v@users.noreply.github.com>
2022-04-28 19:47:50 -0600

    Add files via upload

--- lst_1/GDScls.py
+++ lst_1/GDScls.py
@@ -6,38 +6,46 @@ class GDS(LinearReg):
         super().__init__(file,test_pct)
         self.alpha = alpha
         self.it_num = it_num
+    
+    def GDSstep(self):
+        print(self.get_xTest(1))
+        pass
+    
+    '''   
     def GDSstep(self):
         self.it_list = []
         self.mseLst = []
         self.x = self.get_x(True)["x_test"]
         self.xo =self.get_x()["x_test"]
+        self.xo1 =self.get_x(True)["x_test"]
         self.y = self.get_y()["y_test"]
         y_std = LinearReg.stddize(self.y,False)
         x_std = LinearReg.stddize(self.xo,True)
         self.w=np.zeros((1,x_std.shape[1]))#parameters initial values
-        print(self.w.shape)
         t=0
         while t < self.it_num:
-            t+=1
-            self.it_list.append(t)
             np.random.shuffle(x_std)
-            #print(x_std)
             for i in range(self.x.shape[1]):
-                #print(x_std.shape)
-                #print(self.w.shape)
+                t+=1
+                self.it_list.append(t)
                 self.y_hat = np.sum(x_std[[i],:] * self.w)
-                #self.y_hat = np.array([self.y_hat]).T #y_hat
                 e_std = y_std[[i],:]-self.y_hat#error
-                print(y_std[[i],:].shape)
                 m_err = e_std * x_std[[i],:] * self.alpha #ei x xi
-                #k = m_err.mean(axis=0)*self.alpha #Gradient
                 self.w = self.w + m_err
                 self.y_hato = LinearReg.de_stddize(self.y,self.y_hat)
                 e = (self.y-self.y_hato) 
                 mse = (np.mean(e**2))
                 self.mseLst.append(mse)
-    
-    def GDSshow(self):
+        print(self.xo1)
+        xo1_std = LinearReg.stddize(self.xo1,False)
+        print(self.w)
+        
+        self.y_hat_vec = (np.sum(xo1_std * self.w,axis=1))
+        self.y_hat_vec = LinearReg.de_stddize(self.y_hat_vec,self.y)
+        print(self.y_hat_vec)
+    '''
+
+    #def GDSshow(self):
         #plt.subplot(1,2,1)
         #plt.ylabel("MSE")
         #plt.xlabel("Iterations")
@@ -45,18 +53,18 @@ class GDS(LinearReg):
 
 
         #plt.subplot(1,2,2)
-        plt.ylabel("Y")
-        plt.xlabel("X")
-        plt.scatter(self.xo,self.y)
-        plt.plot(self.xo,self.y_hato)
+        #plt.ylabel("Y")
+        #plt.xlabel("X")
+        #plt.scatter(self.xo,self.y)
+        #plt.plot(self.xo,self.y_hat_vec)
 
-        plt.gcf().canvas.set_window_title('GD')
+        #plt.gcf().canvas.set_window_title('GD')
 
-        plt.show()
+        #plt.show()
 
     
 def main():
-    g = GDS("artificial1d.csv",1,0.01,1000)
+    g = GDS("artificial1d.csv",1,0.1,1000)
     g.GDSstep()
     g.GDSshow()
 
--- lst_1/LinearReg.py
+++ lst_1/LinearReg.py
@@ -2,11 +2,14 @@ import numpy as np
 class LinearReg():
     def __init__(self,file,test_pct=1.0):
         super().__init__()
-        self.file = file
-        self.test_pct = test_pct
         self.dataset = np.genfromtxt(file, delimiter=',', skip_header=0)
         if test_pct < 1.0:
-            np.random.shuffle(dataset)
+            np.random.shuffle(self.dataset)
+        self.x_test = (self.dataset[0:int((self.dataset.shape[0])*test_pct), :])[:, 0:int(self.dataset.shape[1]-1)]
+        self.x_training = (self.dataset[int((self.dataset.shape[0])*test_pct):, :])[:, 0:int(self.dataset.shape[1]-1)] 
+        self.y_test = (self.dataset[0:int((self.dataset.shape[0])*test_pct), :])[:, [-1]] 
+        self.y_training = (self.dataset[int((self.dataset.shape[0])*test_pct):, :])[:, [-1]] 
+        
 
     @staticmethod
     def stddize(matrix,has_one_column : bool):
@@ -24,22 +27,34 @@ class LinearReg():
         mean_vec = (vec.mean(axis=0))*(np.ones((vec.shape[0],vec.shape[1])))
         return (std_dev_vec*vec_hat) + mean_vec
 
-
-
-    def get_x(self, has_one_column : bool = False):
-        x_test = (self.dataset[0:int((self.dataset.shape[0])*self.test_pct), :])[:, 0:int(self.dataset.shape[1]-1)] 
-        x_training = (self.dataset[int((self.dataset.shape[0])*self.test_pct):, :])[:, 0:int(self.dataset.shape[1]-1)] 
-        if(has_one_column):
-            test_one_vec = np.ones((x_test.shape[0],1))
-            x_test = np.concatenate((test_one_vec,x_test),axis = 1)
-            train_one_vec = np.ones((x_training.shape[0],1))
-            x_training = np.concatenate((train_one_vec,x_training),axis = 1)
-        return {"x_test":x_test,"x_training":x_training}
     
-    def get_y(self):
-        y_test = (self.dataset[0:int((self.dataset.shape[0])*self.test_pct), :])[:, [-1]] 
-        y_training = (self.dataset[int((self.dataset.shape[0])*self.test_pct):, :])[:, [-1]] 
-        return {"y_test":y_test,"y_training":y_training}
+    @staticmethod
+    def concat_one_column(matrix):
+        one_vec = np.ones((matrix.shape[0],1))
+        return np.concatenate((one_vec,matrix),axis = 1)
+
+    
+    def get_xTraining(self, has_one_column : bool = False):
+        if(has_one_column):
+            self.x_training = LinearReg.concat_one_column(self.x_training)
+        return self.x_training
+
+
+    def get_xTest(self, has_one_column : bool = False):
+        if(has_one_column):
+            self.x_test = LinearReg.concat_one_column(self.x_test)
+        return self.x_test
+
+    def get_yTest(self):
+        return self.y_test
+    
+    def get_yTraining(self):
+        return self.y_training
+
+    def get_orderN_matrix(self,matrix,order):
+        for i in range(1,order+1):
+            np.concatenate(matrix,matrix**i,axis=1)
+
     
 
 
