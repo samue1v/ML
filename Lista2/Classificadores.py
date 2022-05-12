d57522e0a473c2acfaa50b3585f7f6ff0532bf84
samue1v <samue1v@github.com>
2022-05-12 10:15:11 -0300

    teste

--- Lista2/Classificadores.py
+++ Lista2/Classificadores.py
@@ -27,8 +27,41 @@ class ADG(Classificadores):
             c1_mi = np.mean(indepc1,axis=0)
             m_cov0 = op.sum_covm(indepc0,c0_mi)
             m_cov1 = op.sum_covm(indepc1,c1_mi)
+            pc0 = ADG.ADGprob(x_test_std,m_cov0,c0_mi)
+            pc1 = ADG.ADGprob(x_test_std,m_cov1,c1_mi)            
+            res = []
+            for j in range(len(pc0)):
+                if pc0[j]>pc1[j]:
+                    res.append(0)
+                else:
+                    res.append(1)
+            ac = []
+            #print(y_train.T)
+            for k in range(len(res)):
+                ac.append(res[k]==int(y_test.T[0][k]))
+            print(ac.count(1)/len(ac))
+
+
+            
+    @classmethod
+    def ADGprob(cls,indep,m_cov,mi):
+        prob_vec = []
+        try:
+            inv = np.linalg.inv(m_cov)
+        except:
+            m_cov += np.identity(m_cov.shape[0])*10e-20
+            inv = np.linalg.inv(m_cov)
+
+        for i in range(indep.shape[0]):
+            prob_vec.append(-0.5*np.log(np.linalg.det(m_cov))-0.5*np.array([indep[i]-mi])@inv@(np.array([indep[i]-mi]).T) + np.log(0.5))
+
+        return prob_vec
+
+
+            
     
 def main():
+    #adg = ADG("teste2.csv",2)
     adg = ADG("breastcancer.csv",10)
     adg.ADGstep()
 
